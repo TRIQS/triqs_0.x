91a60c169bb5bc90ec48f9d9dd30b5a209299610
Michel Ferrero <mferrero@cpht.polytechnique.fr>
2012-02-21 17:56:19 +0100

    Small fixes for doc

--- cmake/BuildDoxygenDocs.cmake
+++ cmake/BuildDoxygenDocs.cmake
@@ -5,13 +5,9 @@ string(REPLACE ";" "  " DOXYGEN_SOURCES_LIST "${DOXYGEN_SOURCES}")
 #message( ${DOXYGEN_SOURCES}) 
 
 # Prepare the Doxyfile
-#SET(DOXY_MCTOOLS_BUILD_DIR ${CMAKE_CURRENT_BINARY_DIR}/doxy_${DOXYGEN_NAME})
-#configure_file(${TRIQS_SOURCE_DIR}/cmake/Doxyfile.in ${DOXY_MCTOOLS_BUILD_DIR}/Doxyfile)
 configure_file(${TRIQS_SOURCE_DIR}/cmake/Doxyfile.in ${CMAKE_CURRENT_BINARY_DIR}/Doxyfile)
 
 SET(doxy_top ${CMAKE_CURRENT_BINARY_DIR}/doxy.log)
 add_custom_command (OUTPUT ${doxy_top} DEPENDS ${DOXYGEN_SOURCES} COMMAND ${DOXYGEN_EXECUTABLE} Doxyfile > ${doxy_top} )
 add_custom_target(docs_doxy_${DOXYGEN_NAME} ALL DEPENDS ${doxy_top})
-
-
 
