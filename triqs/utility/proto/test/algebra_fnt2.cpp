f646401e0c79808b14d7905126dfce54985ab590
Olivier Parcollet <olivier.parcollet@cea.fr>
2012-04-22 23:06:47 +0200

    a clean algebra_fnt2 with good proto transform
    Signed-off-by: Olivier Parcollet <olivier.parcollet@cea.fr>

--- triqs/utility/proto/test/algebra_fnt2.cpp
+++ triqs/utility/proto/test/algebra_fnt2.cpp
@@ -27,6 +27,8 @@ struct my_matrix_valued_function {
  my_matrix_valued_function(my_matrix_valued_function const &x): data(x.data) { std::cerr  << "COPY my_matrix_valued_function"<<std::endl ; }
  typedef triqs::arrays::matrix_view<double>  result_type;
  triqs::arrays::matrix_view<double> operator()(size_t i) const { return data(range(),range(),i);}
+ typedef bool D;
+ bool b() const { return true;}
 };
 
 // a trait to identity this type 
@@ -36,12 +38,22 @@ template <>           struct is_a_m_f<my_matrix_valued_function> : mpl::true_ {}
 // a trait to find the scalar of the algebra i.e. the true scalar and the matrix ...
 template <typename T> struct is_scalar_or_element : mpl::or_< triqs::arrays::expressions::matrix_algebra::IsMatrix<T>, triqs::utility::proto::is_in_ZRC<T> > {};
 
-struct LeafGrammar   : proto::and_< proto::terminal<proto::_>, proto::if_<is_a_m_f<proto::_value>()> > {}; 
+struct ElementGrammar: proto::and_< proto::terminal<proto::_>, proto::if_<is_a_m_f<proto::_value>()> > {}; 
 struct ScalarGrammar : proto::and_< proto::terminal<proto::_>, proto::if_<is_scalar_or_element<proto::_value>()> > {}; 
 
-struct myeval {
- BOOST_PROTO_CALLABLE();
+struct algebra_grammar : 
+ proto::or_<
+ ScalarGrammar , ElementGrammar
+ , proto::plus      <algebra_grammar,algebra_grammar>
+ , proto::minus     <algebra_grammar,algebra_grammar>
+ , proto::multiplies<algebra_grammar,algebra_grammar>
+ , proto::divides   <algebra_grammar,algebra_grammar>
+ , proto::negate    <algebra_grammar >
+ > {};
 
+template<int Arity> struct eval_fnt;
+template<> struct eval_fnt<1> {
+ BOOST_PROTO_CALLABLE();
  template<typename Sig> struct result;
  template<typename This, typename F, typename AL> struct result<This(F,AL)>
   : boost::result_of<typename boost::remove_reference<F>::type 
@@ -52,45 +64,58 @@ struct myeval {
   operator ()(F const &f, AL const & al) const { return f(bf::at<mpl::int_<0> >(al)); }
 };
 
-struct Grammar : 
- proto::or_<
- proto::when< ScalarGrammar,                       proto::_value>
- ,proto::when< LeafGrammar,                        myeval(proto::_value, proto::_state) >
- ,proto::when< proto::binary_expr <proto::_,Grammar,Grammar>,      proto::_default<Grammar> > 
-/* ,proto::when< proto::plus <Grammar,Grammar>,      proto::_default<Grammar> > 
- ,proto::when< proto::minus <Grammar,Grammar>,     proto::_default<Grammar> >
- ,proto::when< proto::multiplies<Grammar,Grammar>, proto::_default<Grammar> >
- ,proto::when< proto::divides<Grammar,Grammar>,    proto::_default<Grammar> >
-*/
-  ,proto::when< proto::negate<Grammar >,            proto::_default<Grammar> >
+template<typename Evaluator> struct eval_transform : proto::or_<
+ proto::when<ScalarGrammar, proto::_value>
+ ,proto::when<ElementGrammar, Evaluator(proto::_value, proto::_state) >
+ ,proto::when<proto::nary_expr<proto::_, proto::vararg<proto::_> >,  proto::_default<eval_transform<Evaluator> > >
+ //,proto::when< proto::binary_expr <proto::_,eval_transform<Evaluator>,eval_transform<Evaluator> >, proto::_default<eval_transform<Evaluator> > > 
+ //,proto::when< proto::negate<eval_transform<Evaluator> >, proto::_default<eval_transform<Evaluator> > >
  > {};
 
-struct TL : 
- proto::or_<
- proto::when< ScalarGrammar,                       proto::_value>
- ,proto::when< LeafGrammar,                        myeval(proto::_value, proto::_state) >
- ,proto::when< proto::binary_expr <proto::_,TL,TL>,      proto::_default<Grammar> > 
- ,proto::when< proto::unary_expr<proto::_,TL >,               proto::_default<Grammar> >
+template<typename Accumulator> struct accumulator_transform : proto::or_<
+ proto::when< ScalarGrammar, proto::_state>
+ ,proto::when< ElementGrammar, Accumulator(proto::_value, proto::_state)  >
+ , proto::when<proto::nary_expr<proto::_, proto::vararg<proto::_> >,  proto::fold<proto::_, proto::_state, accumulator_transform<Accumulator> >() >
+ // ,proto::when< proto::binary_expr <proto::_,accumulator_transform,accumulator_transform>, accumulator_transform(proto::_left, accumulator_transform( proto::_right)) >
+ // ,proto::when< proto::unary_expr<proto::_,accumulator_transform >,     accumulator_transform(proto::_left) >
  > {};
 
+//---------------------------
 
+template <typename Expr> struct expr_a_m_f;  // the expression
 
+typedef tup::domain<algebra_grammar,expr_a_m_f,true>  domain_a_m_f; // the domain 
 
-template <typename Expr> struct The_Expr;  // the expression
-
-typedef tup::domain<Grammar,The_Expr,true>  expr_domain; // the domain 
-
-template<typename Expr> struct The_Expr : boost::proto::extends<Expr, The_Expr<Expr>, expr_domain> { // impl the expression
- The_Expr( Expr const & expr = Expr() ) : boost::proto::extends<Expr, The_Expr<Expr>, expr_domain> ( expr ) {}
- template<typename T> typename boost::result_of<Grammar(Expr,bf::vector<T>) >::type 
-  operator() (T const & x) const { return Grammar()(*this, bf::make_vector(x)); }
- friend std::ostream &operator <<(std::ostream &sout, The_Expr<Expr> const &expr) { return boost::proto::eval(expr, triqs::utility::proto::algebra::print_ctx (sout)); }
+template<typename Expr> struct expr_a_m_f : boost::proto::extends<Expr, expr_a_m_f<Expr>, domain_a_m_f> { // impl the expression
+ expr_a_m_f( Expr const & expr = Expr() ) : boost::proto::extends<Expr, expr_a_m_f<Expr>, domain_a_m_f> ( expr ) {}
+ template<typename T> typename boost::result_of<eval_transform<eval_fnt<1> >(Expr,bf::vector<T>) >::type 
+  operator() (T const & x) const { return eval_transform<eval_fnt<1> >()(*this, bf::make_vector(x)); }
+ friend std::ostream &operator <<(std::ostream &sout, expr_a_m_f<Expr> const &expr) { return boost::proto::eval(expr, triqs::utility::proto::algebra::print_ctx (sout)); }
 };
 
-BOOST_PROTO_DEFINE_OPERATORS(is_a_m_f,expr_domain);
+BOOST_PROTO_DEFINE_OPERATORS(is_a_m_f,domain_a_m_f);
 
 template<typename T> void print(T const & x) { std::cout << "any T"<<std::endl ;}
-template<typename A> void print(The_Expr<A> const & x) { std::cout << "an expression "<< x << std::endl ;}
+template<typename A> void print(expr_a_m_f<A> const & x) { std::cout << "an expression "<< x << std::endl ;}
+
+struct myaccu1{
+ BOOST_PROTO_CALLABLE();
+ template<typename Sig> struct result;
+ template<typename This, typename A, typename BFL> struct result<This(A,BFL)> { 
+  typedef bf::cons<typename boost::remove_reference<A>,typename boost::remove_reference<BFL>::type> type;
+ };
+ template<typename A, typename BFL> bf::cons<A,BFL> operator ()(A &a, BFL const & bfl) const { return bf::cons<A,BFL>(a,bfl); }
+ //template<typename A, typename BFL> bf::cons<A,BFL> operator ()(A &a, BFL const & bfl BOOST_PROTO_DISABLE_IF_IS_CONST(Seq)) const { return bf::cons<A,BFL>(a,bfl); }
+};
+
+struct myaccu{
+ BOOST_PROTO_CALLABLE();
+ template<typename Sig> struct result;
+ template<typename This, typename A, typename BFL> 
+  struct result<This(A,BFL)> { typedef bf::cons<bool,typename boost::remove_reference<BFL>::type> type; };
+ template<typename A, typename BFL> bf::cons<bool,BFL> operator ()(A &a, BFL const & bfl) const { return bf::cons<bool,BFL>(a.b(),bfl); }
+};
+
 
 int main() { 
 
@@ -98,12 +123,9 @@ int main() {
  triqs::arrays::matrix<double> A(2,2); A()=0; A(0,0) = 2; A(1,1) = 20;
 
  TEST( (f1 - f2));
-
  TEST(f1(1));
-
  TEST( (2.0* f1 ) (0));
  TEST( (2* f1 + f2) (1));
-
  TEST( triqs::arrays::matrix<double> ( ( 2*f1  +f2 ) (1)) );
  TEST( triqs::arrays::eval ( ( 2*f1  +f2 ) (1)) );
  TEST( triqs::arrays::eval ( ( 2*f1  - f2 ) (1)) );
@@ -117,6 +139,8 @@ int main() {
  print ( f1);
  print ( f1 - f2);
 
+ auto e = 2*f1  + 8*f2;
+ auto l = accumulator_transform<myaccu>()(e, bf::nil() );
+ std::cerr  << triqs::utility::typeid_name(l) <<std::endl;
+
 };
-
-
