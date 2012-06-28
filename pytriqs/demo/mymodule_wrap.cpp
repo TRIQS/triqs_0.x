8069a7e00f172b0ad9ad98f5c80dec72aa45b884
Olivier Parcollet <olivier.parcollet@cea.fr>
2012-06-28 14:02:37 +0200

    update example

--- pytriqs/demo/mymodule_wrap.cpp
+++ pytriqs/demo/mymodule_wrap.cpp
@@ -7,18 +7,49 @@ class myClass
 {
   public:
     myClass(object ob){ U = extract<int>(ob.attr("U")); }
+    myClass (int u): U(u) { }
+
     void solve() { std::cout << "Je suis dans le C++\n" << "U = " << U << std::endl; }
     int U;
 };
 
+
 int increment(int x) { return x+1;}
+
+void read_dict(boost::python::dict d) { 
+
+ int i = extract<int>(d['i']);
+ std::string s = extract<std::string>(d['s']);
+
+ std::cout << "i = " << i<< std::endl ;
+ std::cout << "s = " << s<< std::endl ;
+ 
+ // a more complex example....
+ myClass c = extract<myClass>(d['C']);
+  std::cout  << " myclass .U = " << c.U<< std::endl;
+
+}
+
+
+boost::python::dict modify_dict(boost::python::dict d) { 
+
+  d['i'] *=2;
+ return d;
+}
+
+
+myClass make_myclass(int i) { return myClass(i);} 
 
 BOOST_PYTHON_MODULE(mymodule)
 {
     class_<myClass>("myClass", init<object>())
       .def("solvecpp",&myClass::solve)
+      .add_property("U",&myClass::U)
     ;
 
     def ("inc", increment,"This is my nice inc function ");
+    def ("read_dict", read_dict,"Reading a simple dict ....");
+    def ("modify_dict", modify_dict,"");
+    def ("make_myclass", make_myclass,"");
 
 };
--- pytriqs/demo/solve_python.py
+++ pytriqs/demo/solve_python.py
@@ -1,4 +1,4 @@
-from pytriqs.demo.mymodule import myClass, inc
+from pytriqs.demo.mymodule import *
 
 class Solver:
 
@@ -7,9 +7,21 @@ class Solver:
     myClass(self).solvecpp()
 
 
+
 S = Solver()
 S.U = 11
 S.Solve()
 
 inc (10)
 
+d = dict ( i = 2, s = "a nice string ...", l = [1,2,3])
+d['C'] =  myClass (S)
+
+
+read_dict(d)
+
+print modify_dict (d)
+print d
+
+C= make_myclass(19)
+print C, C.U
