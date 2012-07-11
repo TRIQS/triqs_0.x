d8d4648592a988dc0e20a45244dd4972447c3eeb
Olivier Parcollet <olivier.parcollet@cea.fr>
2012-07-11 15:10:38 +0200

    modified map to have matrix and vector concept ...
    For Thomas...
    
    Signed-off-by: Olivier Parcollet <olivier.parcollet@cea.fr>

--- triqs/arrays/functional/map.hpp
+++ triqs/arrays/functional/map.hpp
@@ -43,7 +43,9 @@ namespace triqs { namespace arrays {
   public :   
   map_impl(F const & f_):f(f_) {}
 
-  template<typename A> class m_result : TRIQS_MODEL_CONCEPT(ImmutableArray) { 
+  template<typename A, typename Enable = void> class m_result;
+
+  template<typename A> class m_result<A,typename boost::enable_if<ImmutableCuboidArray<A> >::type> : TRIQS_MODEL_CONCEPT(ImmutableCuboidArray) { 
     public:
      typedef typename boost::result_of<F(typename A::value_type)>::type value_type;
      typedef typename A::domain_type domain_type;
@@ -54,11 +56,37 @@ namespace triqs { namespace arrays {
      friend std::ostream & operator<<(std::ostream & out, m_result const & x){ return out<<"lazy matrix resulting of a mapping";}
    };
   
+  template<typename A> class m_result<A,typename boost::enable_if<ImmutableMatrix<A> >::type> : TRIQS_MODEL_CONCEPT(ImmutableMatrix) { 
+    public:
+     typedef typename boost::result_of<F(typename A::value_type)>::type value_type;
+     typedef typename A::domain_type domain_type;
+     A const & a; F f;
+     m_result(F const & f_, A const & a_):a(a_),f(f_) {}
+     domain_type domain() const { return a.domain(); } 
+     size_t dim0() const { return a.dim0();}
+     size_t dim1() const { return a.dim1();}
+     value_type operator[] ( typename domain_type::index_value_type const & key) const { return f(a[key]); }
+     friend std::ostream & operator<<(std::ostream & out, m_result const & x){ return out<<"lazy matrix resulting of a mapping";}
+   };
+
+  template<typename A> class m_result<A,typename boost::enable_if<ImmutableVector<A> >::type> : TRIQS_MODEL_CONCEPT(ImmutableVector) { 
+    public:
+     typedef typename boost::result_of<F(typename A::value_type)>::type value_type;
+     typedef typename A::domain_type domain_type;
+     A const & a; F f;
+     m_result(F const & f_, A const & a_):a(a_),f(f_) {}
+     domain_type domain() const { return a.domain(); } 
+     size_t size() const { return a.size();}
+     value_type operator[] ( typename domain_type::index_value_type const & key) const { return f(a[key]); }
+     friend std::ostream & operator<<(std::ostream & out, m_result const & x){ return out<<"lazy matrix resulting of a mapping";}
+   };
+  
+
   template<typename Sig> struct result;
   template<typename This, typename A> struct result<This(A)> { typedef m_result<A> type;};
 
   template< class A > m_result<A> operator()(A const & a) { 
-    static_assert( (ImmutableArray<A>::value), "map : A does not model ImmutableArray");
+    //static_assert( (ImmutableArray<A>::value), "map : A does not model ImmutableArray");
     return m_result<A>(f,a);
    } 
 
