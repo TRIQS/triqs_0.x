75a374df78774f6bb59910b9ab320199cf3a8368
Olivier Parcollet <olivier.parcollet@cea.fr>
2012-10-06 14:40:19 +0200

    arrays : adding lapack_on_view test

--- triqs/arrays/test/C++/lapack_on_view.cpp
+++ triqs/arrays/test/C++/lapack_on_view.cpp
@@ -5,28 +5,25 @@
 #include <iostream>
 #include <triqs/arrays/proto/array_algebra.hpp>
 #include <triqs/arrays/proto/matrix_algebra.hpp>
+#include <triqs/arrays/asserts.hpp>
 
 using std::cout; using std::endl;
 using namespace triqs::arrays;
 using namespace triqs::arrays;
 
+// to be extended to more complex case
+// calling lapack on view to test cache securities....
+//
 int main() { 
   typedef Option::C storage_order;
   // typedef Option::Fortran storage_order;
  
  array<std::complex<double>,3,storage_order> TMPALL (2,2,5); TMPALL()=-1;
  matrix_view<std::complex<double>,storage_order > TMP ( TMPALL (range(), range(), 2));
- matrix<std::complex<double>,storage_order > M1(2,2); M1()=0; M1(0,0) = 2; M1(1,1) = 3.2;
+ matrix<std::complex<double>,storage_order > M1(2,2), Res(2,2); 
+ M1()=0; M1(0,0) = 2; M1(1,1) = 3.2;
+ Res()=0; Res(0,0) = 8; Res(1,1) = 16.64;
  TMP() =0; 
- auto R = M1*M1; // +1.0; // * M1; 
- std::cout << R << std::endl ;
- std::cout << R[mini_vector<size_t,2>(0,0)] << R[mini_vector<size_t,2>(1,1)] << std::endl ;
- TMP() =  R; // M1*( M1 ); 
- std::cerr  << "DEBUG TAIL INVERIN TMP " <<TMP<< std::endl; 
- std::cerr  << "DEBUG TAIL INVERIN TMP " <<TMPALL<< std::endl; 
-
-matrix<std::complex<double>,storage_order > TMP2(2,2); 
-  TMP2() = R;
- std::cerr  << "DEBUG TAIL INVERIN TM2P " <<TMP2<< std::endl; 
-
+ TMP() =   M1*( M1 + 2.0 ); 
+ assert_all_close(TMP(), Res, 1.e-10); 
 }
