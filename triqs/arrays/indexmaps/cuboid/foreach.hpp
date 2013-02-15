4d7b1728958d8df78371a8456bf70648bf2f97ef
Olivier Parcollet <olivier.parcollet@cea.fr>
2013-02-15 23:27:59 +0100

    arrays : work on foreach
    - macro only
    - variadic form
    - speed test ok
    - to do : clean the foreach call for variadic calls.

--- triqs/arrays/indexmaps/cuboid/foreach.hpp
+++ triqs/arrays/indexmaps/cuboid/foreach.hpp
@@ -20,8 +20,9 @@
  ******************************************************************************/
 #ifndef TRIQS_ARRAYS_INDEXMAP_CUBOID_FOREACH_H 
 #define TRIQS_ARRAYS_INDEXMAP_CUBOID_FOREACH_H
-
-//#include <boost/preprocessor/repetition/repeat.hpp>
+#include <boost/preprocessor/repetition/enum_binary_params.hpp>
+#include <boost/preprocessor/repetition/enum.hpp>
+#include <boost/preprocessor/facilities/intercept.hpp>
 //#include <boost/preprocessor/control/if.hpp>
 //#include <boost/preprocessor/arithmetic/sub.hpp>
 //#include <boost/type_traits/add_const.hpp>
@@ -64,7 +65,6 @@ namespace triqs { namespace arrays { namespace indexmaps {
    typedef typename T::value_type v;
    typedef typename boost::mpl::if_<boost::is_const<T>, typename boost::add_const<v>::type,v>::type value_type;
    typedef typename T::indexmap_type indexmap_type;
-   //foreach_impl<indexmap_type, Function, value_type>(x.data_start(),x.indexmap(),F)();
    foreach_impl<indexmap_type, Function, value_type>::invoke(x.data_start(),x.indexmap(),F);
   }
 
@@ -72,28 +72,10 @@ namespace triqs { namespace arrays { namespace indexmaps {
   typename std::enable_if< ! std::is_base_of<Tag::indexmap_storage_pair,Expr>::value >::type 
   foreach( Function F, Expr const & x) { for (auto & pt : x.domain()) boost::unwrap_ref(F)(x[pt],pt); }
 
- /*
- template <typename Expr, typename Function> 
-  typename boost::disable_if<boost::is_base_of<Tag::indexmap_storage_pair,Expr> >::type 
-  foreach( Function F,Expr const & x) {
-   for (typename Expr::domain_type::generator gen = x.domain().begin(); gen; ++gen) {
-    boost::unwrap_ref(F)(x[*gen],*gen);
-   } 
-  }
- */
- 
  //--------------  IMPLEMENTATION -----------------------
- //only cuboid maps is implemented.
- //
-#ifndef NEW_IMPLEMENTATION_FOREACH
-
-//#define AUX0(z,P,NNN) const int p##P = mem_layout::memory_rank_to_index(mem_layout::c_order(Rank),NNN-P); 
-//#define AUX0(z,P,NNN) const int p##P = mem_layout::memory_rank_to_index(mem_layout::c_order(Rank),NNN-P); foreach_int_type * restrict i##P = &t[p##P];
-#define AUX0(z,P,NNN) const int p##P = mem_layout::memory_rank_to_index(mem_layout::fortran_order(Rank),NNN-P);
-//#define AUX0(z,P,NNN) const int p##P = mem_layout::memory_rank_to_index(CM.memory_indices_layout_ull(),NNN-P);
-//#define AUX0(z,P,NNN) const int p##P = mem_layout::memory_rank_to_index(CM.memory_indices_layout_ull(),NNN-P);; foreach_int_type * restrict i##P = &t[p##P];
-//#define AUX0(z,P,NNN) const int p##P = mem_layout::memory_rank_to_index(CM.memory_indices_layout_ull_s(),NNN-P);
-//#define AUX1(z,P,unused) for ( *(i##P)=0; *(i##P)< l[p##P]; ++(*(i##P)))
+ // to do : split into a foreach in ../ and a specialisation here.
+#define AUX0(z,P,NNN) constexpr int p##P = mem_layout::memory_rank_to_index(cuboid::map<Rank,Opt,To>::traversal_order,NNN-P);
+#define AUX0D(z,P,NNN) const int p##P = mem_layout::memory_rank_to_index(CM.memory_indices_layout_ull(),NNN-P);
 #define AUX1(z,P,unused) for (t[p##P]=0; t[p##P]< l[p##P]; ++t[p##P])
 #define AUX2(z,p,unused) BOOST_PP_IF(p,+,) t[p] * s[p] 
 #define IMPL(z, NN, unused)                                \
@@ -107,99 +89,23 @@ namespace triqs { namespace arrays { namespace indexmaps {
    BOOST_PP_REPEAT(BOOST_PP_INC(NN),AUX1,nil)\
    {\
     boost::unwrap_ref(F)( p[BOOST_PP_REPEAT(BOOST_PP_INC(NN),AUX2,nil)], t );\
-   } } };
+   } }\
+  static void invoke_v ( ValueType * restrict p, cuboid::map<Rank,Opt,To> const & CM, Function F) { \
+   mini_vector<foreach_int_type, Rank> t;\
+   BOOST_PP_REPEAT(BOOST_PP_INC(NN),AUX0,NN)\
+   const mini_vector<foreach_int_type, Rank>  l(CM.lengths());\
+   const mini_vector<foreach_int_type, Rank>  s(CM.strides());\
+   BOOST_PP_REPEAT(BOOST_PP_INC(NN),AUX1,nil)\
+   {\
+    boost::unwrap_ref(F)( p(BOOST_PP_ENUM_BINARY_PARAMS(BOOST_PP_INC(NN),t[p,] BOOST_PP_INTERCEPT)), t );\
+   } }\
+ };
  BOOST_PP_REPEAT(ARRAY_NRANK_MAX , IMPL, nil);
 #undef IMPL
 #undef AUX0
 #undef AUX1
 #undef AUX2
 #undef PP
-
-#else
-
-  namespace mem_layout {
-   int memory_rank_to_index2(ull_t p, int r) { return r;} 
-  }
-
- template<int Rank, ull_t Opt, ull_t To, typename Function, typename ValueType>
-  struct foreach_impl <cuboid::map<Rank,Opt,To>,Function,ValueType,void > {
-   typedef std::ptrdiff_t int_type; 
-   mini_vector<int_type, Rank> t;
-   
-   // better to be signed here : 1) on some machine/compiler, it is a lot faster !
-   // When used with clef auto assign, e.g. A(i_,j_) = i -2*j, one needs signed arithmetics
-   // The clef adapters would convert, but this requires a conversion at each call....
-   // typedef size_t int_type;
-
-   ValueType * restrict p; cuboid::map<Rank,Opt,To> const & CM; Function F;
-   int indices[Rank];
-   
-   const mini_vector<int_type, Rank>  l,s;
- 
-   foreach_impl(ValueType * restrict p_, cuboid::map<Rank,Opt,To> const & CM_, Function F_): p(p_),CM(CM_),F(F_),
-   l(CM.lengths()), s(CM.strides()) {
-    for (int i=0; i<Rank; ++i) indices[i] = i;//mem_layout::memory_rank_to_index(CM.memory_indices_layout().value,i);     
-   }
-
-   /*
-   inline void operator()() { impl(std::integral_constant<int,Rank>(),0);}
-
-   // reverse Rank and Rank -v
-   template<int v> inline void impl ( std::integral_constant<int,v>, int_type const & ind) { 
-    int u = mem_layout::memory_rank_to_index(CM.memory_indices_layout().value,Rank - v);
-    for (t[u]=0; t[u] < CM.lengths()[u]; ++t[u]) impl(std::integral_constant<int,v-1>(), ind + t[u]* CM.strides()[u]);
-   }
-   inline void impl ( std::integral_constant<int,0>, int_type const & ind) { boost::unwrap_ref(F)( p[ind], t); }
-*/
-   
-//   inline void operator()() { impl(std::integral_constant<int,Rank>(),0);}
- 
-#ifndef WITH_T 
-   inline void operator()() {
-    //int u = 0;//mem_layout::memory_rank_to_index(CM.memory_indices_layout().value,0);
-    //int u = mem_layout::memory_rank_to_index(CM.memory_indices_layout().value,0);
-    int u = indices[0];
-    //for (int_type x=0; x < CM.lengths()[u]; ++x) {  fe_impl(std::integral_constant<int,Rank-1>(), x* CM.strides()[u]);}
-    //for (int_type x=0; x < CM.lengths()[u]; ++x) { t[u] = x; fe_impl(std::integral_constant<int,Rank-1>(), x* CM.strides()[u]);}
-    for (t[u]=0; t[u] < CM.lengths()[u]; ++t[u]) fe_impl(std::integral_constant<int,Rank-1>(), t[u]* CM.strides()[u]);
-   } 
-
-
-   // reverse Rank and Rank -v
-   template<int v> inline void fe_impl ( std::integral_constant<int,v>, int_type const & ind) { 
-    //int u = Rank-v;//mem_layout::memory_rank_to_index(CM.memory_indices_layout().value,Rank - v);
-    //int u = mem_layout::memory_rank_to_index(CM.memory_indices_layout().value,Rank - v);
-    int u = indices[Rank-v];
-    //for (int_type x=0; x < CM.lengths()[u]; ++x) {  fe_impl(std::integral_constant<int,v-1>(), ind + x* CM.strides()[u]);}
-    //for (int_type x=0; x < CM.lengths()[u]; ++x) { t[u] = x; fe_impl(std::integral_constant<int,v-1>(), ind + x* CM.strides()[u]);}
-    for (t[u]=0; t[u] < CM.lengths()[u]; ++t[u]) fe_impl(std::integral_constant<int,v-1>(), ind + t[u]* CM.strides()[u]);
-   }
-   inline void fe_impl ( std::integral_constant<int,0>, int_type const & ind) { boost::unwrap_ref(F)( p[ind], t); }
-
-#else
-
-  inline void operator()() {
-    //int u = indices[0];
-    int u = mem_layout::memory_rank_to_index(CM.memory_indices_layout().value,0);
-    //for (int_type x=0; x < CM.lengths()[u]; ++x) { fe_impl(std::integral_constant<int,Rank-1>(), x* CM.strides()[u],x);}
-    for (int_type x=0; x < l[u]; ++x) { fe_impl(std::integral_constant<int,Rank-1>(), x* s[u],x);}
-   } 
-
-  template<int v, typename ... Args>
-    void fe_impl ( std::integral_constant<int,v>, int_type ind,  Args const & ... args) { 
-    //int u = indices[Rank-v];
-    int u = mem_layout::memory_rank_to_index(CM.memory_indices_layout().value,Rank - v);
-     for (int_type x=0; x < l[u]; ++x) { fe_impl(std::integral_constant<int,v-1>(), ind + x* s[u], args...,x); }
-     //for (int_type x=0; x < CM.lengths()[u]; ++x) { fe_impl(std::integral_constant<int,v-1>(), ind + x* CM.strides()[u], args...,x); }
-    }
-
-   template<typename ... Args>
-    void fe_impl ( std::integral_constant<int,0>, int_type ind, Args const & ... args) { boost::unwrap_ref(F)( p[ind], args... ); }
-
-#endif
-
-  };
-#endif
 
 }}}//namespace
 #endif
