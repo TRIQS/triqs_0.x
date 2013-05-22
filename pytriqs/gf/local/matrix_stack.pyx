2990e0bc9c9974ce948ed8b7b7bc4d5791bf81ff
Michel Ferrero <mferrero@cpht.polytechnique.fr>
2013-05-22 16:28:16 +0200

    Add cast from double to complex in matrix_stack
    This allows to do an operation like
    
      G += numpy.array([[1.0,1.0],[1.0,1.0], numpy.double)
    
    Even if G.data is complex.
    
      modified:   pytriqs/gf/local/matrix_stack.pyx

--- pytriqs/gf/local/matrix_stack.pyx
+++ pytriqs/gf/local/matrix_stack.pyx
@@ -12,19 +12,20 @@ class MatrixStack:
         if self.is_double:
           matrix_stack[double](self.data).iadd(matrix_view[double](M))
         else:
-          matrix_stack[dcomplex](self.data).iadd(matrix_view[dcomplex](M))
+          matrix_stack[dcomplex](self.data).iadd(matrix_view[dcomplex](M.astype(numpy.complex)))
 
     def sub(self, M):
         if self.is_double:
           matrix_stack[double](self.data).isub(matrix_view[double](M))
         else:
-          matrix_stack[dcomplex](self.data).isub(matrix_view[dcomplex](M))
+          matrix_stack[dcomplex](self.data).isub(matrix_view[dcomplex](M.astype(numpy.complex)))
 
     def matmul_L_R(self, L, D, R):
         if self.is_double:
           matrix_stack[double](self.data).onsite_matmul_L_R(matrix_view[double](L), matrix_stack[double](D), matrix_view[double](R))
         else:
-          matrix_stack[dcomplex](self.data).onsite_matmul_L_R(matrix_view[dcomplex](L), matrix_stack[dcomplex](D), matrix_view[dcomplex](R))
+          matrix_stack[dcomplex](self.data).onsite_matmul_L_R(
+            matrix_view[dcomplex](L.astype(numpy.complex)), matrix_stack[dcomplex](D), matrix_view[dcomplex](R.astype(numpy.complex)))
 
     def invert(self):
         if self.is_double:
