a250af2786e5e4c6c1181b67b74b73ce45a61de1
Michel Ferrero <mferrero@cpht.polytechnique.fr>
2013-06-10 15:27:15 +0200

    Improve fix for B/F mesh_imfreq
    This is maybe a slightly cleaner solution to figure the
    statistics of a mesh.
    
      modified:   pytriqs/gf/local/mesh_imfreq.pyx

--- pytriqs/gf/local/mesh_imfreq.pyx
+++ pytriqs/gf/local/mesh_imfreq.pyx
@@ -28,5 +28,6 @@ cdef class MeshImFreq:
 
 # C -> Python 
 cdef inline make_MeshImFreq (mesh_imfreq x) :
-    return MeshImFreq( x.domain().beta, 'F' if x.x_min().real>0 else 'B', x.size() ) #change True -> ? x.x_min()>0 does not work
+    return MeshImTime( x.domain().beta, 'F' if x.domain().statistic==Fermion else 'B', x.size() )
+
 
